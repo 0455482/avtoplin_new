24e1ce3494ad7b2808fa6bd3eede00cfa949f70b
= <=>
2016-05-03 14:52:23 +0200

    updates

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -45,94 +45,95 @@
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>resources/libs/css/ng-animation.css">
 </head>
 
-<header class="navbar navbar-default navbar-static-top hidden-print ng-scope">
-	<!-- start: TOP NAVBAR -->
-	<!-- start: NAVBAR HEADER -->
-	<div class="navbar-header">
-		<a href="#" class="sidebar-mobile-toggler pull-left hidden-md hidden-lg" ng-click="toggle('sidebar')" class="btn btn-navbar sidebar-toggle"> <i class="ti-align-justify"></i> </a>
-		<a class="navbar-brand" ui-sref="app.dashboard"> <div>{{app.name}}</div> </a>
-		<a href="#" class="sidebar-toggler pull-right visible-md visible-lg" ng-click="app.layout.isSidebarClosed = !app.layout.isSidebarClosed"> <i class="ti-align-justify"></i> </a>
-		<a class="pull-right menu-toggler visible-xs-block" id="menu-toggler" ng-click="navbarCollapsed = !navbarCollapsed"> <span class="sr-only">Toggle navigation</span> <i class="ti-view-grid"></i> </a>
-	</div>
-	<!-- end: NAVBAR HEADER -->
-	<!-- start: NAVBAR COLLAPSE -->
-	<div class="navbar-collapse collapse" uib-collapse="navbarCollapsed" ng-init="navbarCollapsed = true" off-click="navbarCollapsed = true" off-click-if='!navbarCollapsed' off-click-filter="#menu-toggler">
-		<ul class="nav navbar-right" ct-fullheight="window" data-ct-fullheight-exclusion="header" data-ct-fullheight-if="isSmallDevice">
-			<!-- start: MESSAGES DROPDOWN -->
-			<!-- /// controller:  'InboxCtrl' -  localtion: assets/js/controllers/InboxCtrl.js /// -->
-
-			<!-- end: MESSAGES DROPDOWN -->
-			<!-- start: ACTIVITIES DROPDOWN -->
-			<li class="dropdown" uib-dropdown on-toggle="toggled(open)">
-				<a href class="dropdown-toggle" uib-dropdown-toggle> <i class="ti-check-box"></i> <span translate="topbar.activities.MAIN">ACTIVITIES</span> </a>
-				<ul class="dropdown-menu dropdown-light dropdown-messages dropdown-large">
-					<li>
-						<span class="dropdown-header" translate="topbar.activities.HEADER"> You have new notifications</span>
-					</li>
-					<li>
-						<div class="drop-down-wrapper ps-container">
-							<div class="list-group no-margin">
-								<a class="media list-group-item" href=""> <img class="img-circle" alt="..." src="../STANDARD/assets/images/avatar-1.jpg"> <span class="media-body block no-margin"> Use awesome animate.css <small class="block text-grey">10 minutes ago</small> </span> </a>
-								<a class="media list-group-item" href=""> <span class="media-body block no-margin"> 1.0 initial released <small class="block text-grey">1 hour ago</small> </span> </a>
-							</div>
-						</div>
-					</li>
-					<li class="view-all">
-						<a href="#" translate="topbar.activities.SEEALL"> See All </a>
-					</li>
-				</ul>
-			</li>
-			<!-- end: ACTIVITIES DROPDOWN -->
-			<!-- start: LANGUAGE SWITCHER -->
-			<li class="dropdown" uib-dropdown on-toggle="toggled(open)">
-				<a href class="dropdown-toggle" uib-dropdown-toggle> <i class="ti-world"></i> {{language.selected}} </a>
-				<ul role="menu" class="dropdown-menu dropdown-light fadeInUpShort">
-					<li ng-repeat="(localeId, langName) in language.available">
-						<a ng-click="language.set(localeId, $event)" href="#" class="menu-toggler"> {{langName}} </a>
-					</li>
-				</ul>
-			</li>
-			<!-- start: LANGUAGE SWITCHER -->
-			<!-- start: USER OPTIONS DROPDOWN -->
-			<li class="dropdown current-user" uib-dropdown on-toggle="toggled(open)">
-				<a href class="dropdown-toggle" uib-dropdown-toggle> <img src="../STANDARD/assets/images/avatar-1.jpg" alt="{{user.name}}"> <span class="username">{{user.name}} <i class="ti-angle-down"></i></i></span> </a>
-				<ul class="dropdown-menu dropdown-dark">
-					<li>
-						<a ui-sref="app.pages.user" translate="topbar.user.PROFILE"> My Profile </a>
-					</li>
-					<li>
-						<a ui-sref="app.pages.calendar" translate="topbar.user.CALENDAR"> My Calendar </a>
-					</li>
-					<li>
-						<a ui-sref="app.pages.messages" translate="topbar.user.MESSAGES"> My Messages (3) </a>
-					</li>
-					<li>
-						<a ui-sref="login.lockscreen" translate="topbar.user.LOCKSCREEN"> Lock Screen </a>
-					</li>
-					<li>
-						<a ui-sref="login.signin" translate="topbar.user.LOGOUT"> Log Out </a>
-					</li>
-				</ul>
-			</li>
-			<!-- end: USER OPTIONS DROPDOWN -->
-		</ul>
-		<!-- start: MENU TOGGLER FOR MOBILE DEVICES -->
-		<div class="close-handle visible-xs-block menu-toggler" ng-click="navbarCollapsed = true">
-			<div class="arrow-left"></div>
-			<div class="arrow-right"></div>
-		</div>
-		<!-- end: MENU TOGGLER FOR MOBILE DEVICES -->
-	</div>
-	<a class="sidebar-mobile-toggler dropdown-off-sidebar hidden-md hidden-lg" ng-click="toggle('off-sidebar')"> &nbsp; </a>
-	<a class="dropdown-off-sidebar hidden-sm hidden-xs" ng-click="toggle('off-sidebar')"> &nbsp; </a>
-	<!-- end: NAVBAR COLLAPSE -->
-	<!-- start: TOP NAVBAR -->
-
-</header>
-
 
 <!-- start: SEARCH FORM -->
 <div ng-class="{'app-mobile' : app.isMobile, 'app-navbar-fixed' : app.layout.isNavbarFixed, 'app-sidebar-fixed' : app.layout.isSidebarFixed, 'app-sidebar-closed':app.layout.isSidebarClosed, 'app-footer-fixed':app.layout.isFooterFixed}">
+	<div class="app-content ng-scope">
+		<header class="navbar navbar-default navbar-static-top hidden-print ng-scope">
+			<!-- start: TOP NAVBAR -->
+			<!-- start: NAVBAR HEADER -->
+			<div class="navbar-header">
+				<a href="#" class="sidebar-mobile-toggler pull-left hidden-md hidden-lg" ng-click="toggle('sidebar')" class="btn btn-navbar sidebar-toggle"> <i class="ti-align-justify"></i> </a>
+				<a class="navbar-brand" ui-sref="app.dashboard"> <div>{{app.name}}</div> </a>
+				<a href="#" class="sidebar-toggler pull-right visible-md visible-lg" ng-click="app.layout.isSidebarClosed = !app.layout.isSidebarClosed"> <i class="ti-align-justify"></i> </a>
+				<a class="pull-right menu-toggler visible-xs-block" id="menu-toggler" ng-click="navbarCollapsed = !navbarCollapsed"> <span class="sr-only">Toggle navigation</span> <i class="ti-view-grid"></i> </a>
+			</div>
+			<!-- end: NAVBAR HEADER -->
+			<!-- start: NAVBAR COLLAPSE -->
+			<div class="navbar-collapse collapse" uib-collapse="navbarCollapsed" ng-init="navbarCollapsed = true" off-click="navbarCollapsed = true" off-click-if='!navbarCollapsed' off-click-filter="#menu-toggler">
+				<ul class="nav navbar-right" ct-fullheight="window" data-ct-fullheight-exclusion="header" data-ct-fullheight-if="isSmallDevice">
+					<!-- start: MESSAGES DROPDOWN -->
+					<!-- /// controller:  'InboxCtrl' -  localtion: assets/js/controllers/InboxCtrl.js /// -->
+
+					<!-- end: MESSAGES DROPDOWN -->
+					<!-- start: ACTIVITIES DROPDOWN -->
+					<li class="dropdown" uib-dropdown on-toggle="toggled(open)">
+						<a href class="dropdown-toggle" uib-dropdown-toggle> <i class="ti-check-box"></i> <span translate="topbar.activities.MAIN">ACTIVITIES</span> </a>
+						<ul class="dropdown-menu dropdown-light dropdown-messages dropdown-large">
+							<li>
+								<span class="dropdown-header" translate="topbar.activities.HEADER"> You have new notifications</span>
+							</li>
+							<li>
+								<div class="drop-down-wrapper ps-container">
+									<div class="list-group no-margin">
+										<a class="media list-group-item" href=""> <img class="img-circle" alt="..." src="../STANDARD/assets/images/avatar-1.jpg"> <span class="media-body block no-margin"> Use awesome animate.css <small class="block text-grey">10 minutes ago</small> </span> </a>
+										<a class="media list-group-item" href=""> <span class="media-body block no-margin"> 1.0 initial released <small class="block text-grey">1 hour ago</small> </span> </a>
+									</div>
+								</div>
+							</li>
+							<li class="view-all">
+								<a href="#" translate="topbar.activities.SEEALL"> See All </a>
+							</li>
+						</ul>
+					</li>
+					<!-- end: ACTIVITIES DROPDOWN -->
+					<!-- start: LANGUAGE SWITCHER -->
+					<li class="dropdown" uib-dropdown on-toggle="toggled(open)">
+						<a href class="dropdown-toggle" uib-dropdown-toggle> <i class="ti-world"></i> {{language.selected}} </a>
+						<ul role="menu" class="dropdown-menu dropdown-light fadeInUpShort">
+							<li ng-repeat="(localeId, langName) in language.available">
+								<a ng-click="language.set(localeId, $event)" href="#" class="menu-toggler"> {{langName}} </a>
+							</li>
+						</ul>
+					</li>
+					<!-- start: LANGUAGE SWITCHER -->
+					<!-- start: USER OPTIONS DROPDOWN -->
+					<li class="dropdown current-user" uib-dropdown on-toggle="toggled(open)">
+						<a href class="dropdown-toggle" uib-dropdown-toggle> <img src="../STANDARD/assets/images/avatar-1.jpg" alt="{{user.name}}"> <span class="username">{{user.name}} <i class="ti-angle-down"></i></i></span> </a>
+						<ul class="dropdown-menu dropdown-dark">
+							<li>
+								<a ui-sref="app.pages.user" translate="topbar.user.PROFILE"> My Profile </a>
+							</li>
+							<li>
+								<a ui-sref="app.pages.calendar" translate="topbar.user.CALENDAR"> My Calendar </a>
+							</li>
+							<li>
+								<a ui-sref="app.pages.messages" translate="topbar.user.MESSAGES"> My Messages (3) </a>
+							</li>
+							<li>
+								<a ui-sref="login.lockscreen" translate="topbar.user.LOCKSCREEN"> Lock Screen </a>
+							</li>
+							<li>
+								<a ui-sref="login.signin" translate="topbar.user.LOGOUT"> Log Out </a>
+							</li>
+						</ul>
+					</li>
+					<!-- end: USER OPTIONS DROPDOWN -->
+				</ul>
+				<!-- start: MENU TOGGLER FOR MOBILE DEVICES -->
+				<div class="close-handle visible-xs-block menu-toggler" ng-click="navbarCollapsed = true">
+					<div class="arrow-left"></div>
+					<div class="arrow-right"></div>
+				</div>
+				<!-- end: MENU TOGGLER FOR MOBILE DEVICES -->
+			</div>
+			<a class="sidebar-mobile-toggler dropdown-off-sidebar hidden-md hidden-lg" ng-click="toggle('off-sidebar')"> &nbsp; </a>
+			<a class="dropdown-off-sidebar hidden-sm hidden-xs" ng-click="toggle('off-sidebar')"> &nbsp; </a>
+			<!-- end: NAVBAR COLLAPSE -->
+			<!-- start: TOP NAVBAR -->
+
+		</header>
+	</div>
 	<div class="sidebar app-aside hidden-print ng-scope" id="sidebar" toggleable parent-active-class="app-slide-off">
 		<div perfect-scrollbar wheel-propagation="false" suppress-scroll-x="true" class="sidebar-container ps-container ps-active-y">
 			<nav>
@@ -158,186 +159,6 @@
 					<span>Main Navigation</span>
 				</div>
 				<ul class="main-navigation-menu">
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Dashboard </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Nastavitve </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Statistika </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Dashboard </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Nastavitve </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Statistika </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Dashboard </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Nastavitve </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Statistika </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Dashboard </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Nastavitve </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Statistika </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Dashboard </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Nastavitve </span>
-								</div>
-							</div>
-						</a>
-					</li>
-					<li ui-sref-active="active">
-						<a ui-sref="app.dashboard">
-							<div class="item-content">
-								<div class="item-media">
-									<i class="ti-home"></i>
-								</div>
-								<div class="item-inner">
-									<span class="title"> Statistika </span>
-								</div>
-							</div>
-						</a>
-					</li>
 					<li ui-sref-active="active">
 						<a ui-sref="app.dashboard">
 							<div class="item-content">
