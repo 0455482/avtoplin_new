650feccd8d22e60dfdbba5eee5852cbb5939dcc6
= <=>
2016-05-05 09:11:45 +0200

    updates

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -18,54 +18,21 @@
     <!-- start: FEATURED BOX LINKS -->
     <div class="container-fluid container-fullw bg-white">
       <div class="row">
-        <div class="col-sm-4">
-          <div class="panel panel-white no-radius text-center">
-            <div class="panel-body">
-              <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i> </span>
-              <h2 class="StepTitle">Manage Users</h2>
-              <p class="text-small">
-                To add users, you need to be signed in as the super user.
-              </p>
-              <p class="links cl-effect-1">
-                <a href>
-                  view more
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="panel panel-white no-radius text-center">
-            <div class="panel-body">
-              <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-paperclip fa-stack-1x fa-inverse"></i> </span>
-              <h2 class="StepTitle">Manage Orders</h2>
-              <p class="text-small">
-                The Manage Orders tool provides a view of all your orders.
-              </p>
-              <p class="cl-effect-1">
-                <a href>
-                  view more
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="panel panel-white no-radius text-center">
-            <div class="panel-body">
-              <span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-terminal fa-stack-1x fa-inverse"></i> </span>
-              <h2 class="StepTitle">Manage Database</h2>
-              <p class="text-small">
-                Store, modify, and extract information from your database.
-              </p>
-              <p class="links cl-effect-1">
-                <a href>
-                  view more
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
+        <div class="col-md-12">
+    			<h5 class="over-title margin-bottom-15"><span class="text-bold">3D rolling</span> links</h5>
+    			<p>
+    				Enclose your link inside the class <code>.cl-effect-2</code>
+    			</p>
+    			<div class="margin-top-30 margin-bottom-30">
+    				<nav id="cl-effect-2" class="links cl-effect-2">
+    					<a href><span data-hover="Ratatouille">Ratatouille</span></a>
+    					<a href><span data-hover="Lassitude">Lassitude</span></a>
+    					<a href><span data-hover="Murmurous">Murmurous</span></a>
+    					<a href><span data-hover="Palimpsest">Palimpsest</span></a>
+    					<a href><span data-hover="Assemblage">Assemblage</span></a>
+    				</nav>
+    			</div>
+    		</div>
       </div>
     </div>
     <!-- end: FEATURED BOX LINKS -->
